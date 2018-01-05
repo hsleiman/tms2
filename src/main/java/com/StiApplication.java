095012f92dadb6ca9c2c7e8db720361e3d6606b4
Bishistha <Bishistha@172.24.65.99>
2018-01-04 19:58:06 -0800

    propertysource for stiapplication

--- src/main/java/com/StiApplication.java
+++ src/main/java/com/StiApplication.java
@@ -2,7 +2,12 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
+@PropertySources({
+    @PropertySource( value = "classpath:application.properties" )
+})
 @SpringBootApplication
 public class StiApplication {
 
