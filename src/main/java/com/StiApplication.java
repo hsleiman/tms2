f848623e1b845b1ca6b781d63d6159a6f6f2bfcf
Hussien Sleiman <hussien.sleiman@objectbrains.com>
2018-01-04 21:59:59 -0800

    test

--- src/main/java/com/StiApplication.java
+++ src/main/java/com/StiApplication.java
@@ -1,14 +1,20 @@
 package com;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @PropertySources({
     @PropertySource( value = "classpath:application.properties" )
 })
 @SpringBootApplication
+@EnableJpaRepositories
+@EnableAutoConfiguration
+@EnableTransactionManagement
 public class StiApplication {
 
 	public static void main(String[] args) {
