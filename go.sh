44a1eac8a5a235c8462eaa2d2473af0f081e7e36
Bishistha <Bishistha@172.24.65.99>
2018-01-05 16:45:50 -0800

    dependencies fix

--- go.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-gradle clean
-gradle build --debug --stacktrace
-
-echo read -p "Press enter to continue"
-
-cp ./build/libs/*.war /Users/hsleiman/CloudStation/Servers/apache-tomcat-8.0.30/webapps
